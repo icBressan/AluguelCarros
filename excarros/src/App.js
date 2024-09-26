90ca7465bfedc7f0ea3e1f0a63819209e98c21c4
Isabelle <ibressan@yahoo.com.br>
2024-09-26 12:28:33 -0300

    primeiro commit

---
+++ excarros/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+
+  // Declaração de objetos
+  const carro1 = { nome: "Sedan", preco: 150.0 };
+  const carro2 = { nome: "SUV", preco: 200.0 };
+  const carro3 = { nome: "Economico", preco: 100.0 };
+  const carro4 = { nome: "Conversível", preco: 300.0 };
+  const carro5 = { nome: "Picape", preco: 250.0 };
+  const carro6 = { nome: "Esportivo", preco: 400.0 };
+
+  // Declaração de array (vetor)
+  const modelo = [carro1,carro2,carro3,carro4,carro5,carro6];
+
+  
+
+  return (
+    
+  );
+}
+
+export default App;
